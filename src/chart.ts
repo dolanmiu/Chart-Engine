260db6f42ff571180fc6687257f7cc12ada21d63
Dolan Miu <dolan_miu@hotmail.com>
2016-11-14 21:51:25 +0000

    work on series

--- src/chart.ts
+++ src/chart.ts
@@ -1,15 +1,21 @@
 import * as PIXI from "pixi.js";
+import { Series } from "./series";
 
 export class Chart {
     private renderer: PIXI.CanvasRenderer | PIXI.WebGLRenderer;
     private stage: PIXI.Container;
+    private seriesCollection: Array<Series>;
+
+    constructor() {
+        this.seriesCollection = new Array<Series>();
+    }
 
     init() {
         this.renderer = PIXI.autoDetectRenderer(800, 600, { backgroundColor: 0x1099bb });
         document.body.appendChild(this.renderer.view);
 
         this.stage = new PIXI.Container();
-        let texture = PIXI.Texture.fromImage('../src/assets/bunny.png');
+        let texture = PIXI.Texture.fromImage("../src/assets/bunny.png");
         let bunny = new PIXI.Sprite(texture);
         bunny.anchor.x = 0.5;
         bunny.anchor.y = 0.5;
@@ -20,12 +26,15 @@ export class Chart {
         this.stage.addChild(bunny);
     }
 
-    animate = () => {
+    public animate = () => {
         requestAnimationFrame(this.animate);
         let bunny = this.stage.getChildAt(0);
         bunny.rotation += 0.01;
         this.renderer.render(this.stage);
     };
 
-}
+    public addSeries(series: Series) {
+        this.seriesCollection.push(series);
+    }
 
+}
