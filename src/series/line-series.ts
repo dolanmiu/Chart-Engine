3456b834f365817c7ce2a491bd2a1eca27b8a769
Dolan Miu <dolan_miu@hotmail.com>
2016-11-28 01:32:59 +0000

    more work to series

--- src/series/line-series.ts
+++ src/series/line-series.ts
@@ -1,3 +1,24 @@
-export class LineSeries {
+import * as PIXI from "pixi.js";
+import { ISeriesNode } from "./node";
+import { ICoordinate } from "./coordinate";
 
+export class LineSeries extends PIXI.Graphics implements ISeriesNode {
+
+    private coordinates: Array<ICoordinate>;
+
+    constructor() {
+        super();
+    }
+
+    draw() {
+        this.clear();
+
+        this.coordinates.forEach((coordinate, i) => {
+            if (i === 0) {
+                this.moveTo(coordinate.x, coordinate.y);
+            }
+
+            this.lineTo(coordinate.x, coordinate.y);
+        });
+    }
 }
--- src/series/node/linear-node.ts
+++ src/series/node/linear-node.ts
@@ -1,21 +1,21 @@
 import * as PIXI from "pixi.js";
-import { ICoordinate } from "../coordinate";
 import { ISeriesNode } from "./node";
 
 export abstract class LinearNode extends PIXI.Graphics implements ISeriesNode {
     private xAxisPos: number;
-    private coordinates: Array<ICoordinate>;
+    private coordinates: Array<number>;
 
     constructor(x: number) {
         super();
-        this.coordinates = new Array<ICoordinate>();
+        this.coordinates = new Array<number>();
         this.xAxisPos = x;
     }
 
     draw() {
         this.clear();
+
         for (let coordinate of this.coordinates) {
-            this.moveTo(this.xAxisPos, coordinate.y);
+            this.moveTo(this.xAxisPos, coordinate);
         }
     }
 }
