8f2c117ed71ca4a9f661065d21eabc9e7eb5fc08
Dolan Miu <dolan_miu@hotmail.com>
2016-11-28 01:57:18 +0000

    added padding to the date namer

--- src/common/to-stringer/date-to-stringer.ts
+++ src/common/to-stringer/date-to-stringer.ts
@@ -2,7 +2,17 @@ import { IToStringer } from "./to-stringer";
 
 export class DateToStringer implements IToStringer<Date> {
     stringify(date: Date) {
-        let str = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+        let hours = this.pad(date.getHours(), 2);
+        let minutes = this.pad(date.getMinutes(), 2);
+        let seconds = this.pad(date.getSeconds(), 2);
+        let str = `${hours}:${minutes}:${seconds}`;
+
         return str;
+    }
+
+    private pad(num: number, size: number) {
+        let str = "00" + num;
+
+        return str.substr(str.length - size);
     }
 }
